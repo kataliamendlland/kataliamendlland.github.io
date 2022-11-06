64bb7c747f9e54759c3ba8dcbaba9730b28f2208
twbjelland <60273828+twbjelland@users.noreply.github.com>
2022-11-06 15:29:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,15 @@ function onLoad(){
     var path = "/media/"+rand+".png";
 
     var screenWidth = window.innerWidth;
-    console.log(screenWidth);
     var screenHeight = window.innerHeight;
-    console.log(screenHeight);
 
     var randX = Math.floor(Math.random() * (screenWidth - 100));
     var randY = Math.floor(Math.random() * (screenHeight - 100));
-    console.log(randY);
 
     var img = document.createElement('img');
     img.setAttribute('src', path);
     img.setAttribute('height', '500vh');
     img.setAttribute('style', 'position: absolute; top: '+randY+'px; left: '+randX+'px;');
-    document.body.appendChild(img);
 }
 
 function onClick(event){
