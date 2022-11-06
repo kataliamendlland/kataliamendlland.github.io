345c03fc992a0fbc6521f3f97c56e5898de6cab2
twbjelland <60273828+twbjelland@users.noreply.github.com>
2022-11-06 14:29:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const FRIENDS_NUM = 3;
 
 function onLoad(){
-  var rand = Math.floor(Math.random() * FRIENDS_NUM);
+  var rand = Math.floor(Math.random() * FRIENDS_NUM)+1;
   alert("You have "+rand+" friends");
   var path = "/media/"+rand+".png";
   var img = document.createElement('img');
