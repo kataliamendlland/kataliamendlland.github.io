913320f2dfbfdd6d9704b18c93cc1434dd446f03
twbjelland <60273828+twbjelland@users.noreply.github.com>
2022-11-06 14:47:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ function onLoad(){
   var path = "/media/"+rand+".png";
   
   var screenWidth = window.innerWidth;
+  console.log(screenWidth);
   var screenHeight = window.innerHeight;
+  console.log(screenHeight);
   
   var randX = Math.floor(Math.random() * (screenWidth - 1000));
   var randY = Math.floor(Math.random() * (screenHeight - 1000));
+  console.log(randY);
   
   var img = document.createElement('img');
   img.setAttribute('src', path);
